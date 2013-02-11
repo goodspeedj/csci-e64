9622669f5e7d8a7521dfcce4788d8b81bcd8e204
goodspeed <JGoodspeed@bottomline.com>
2013-02-11 16:45:44 -0500

    Most fields working - still need to figure out the hash tags

--- homework/hw2/API_scraper.py
+++ homework/hw2/API_scraper.py
@@ -14,7 +14,7 @@
 		#	Text_of_tweet (as a string)	
 		#	Hashtag1 (if any) with first hashtag word without hashtag symbol in front of it
 		#	Hashtag2 (if ang)
-		#	….
+		#	
 		
 # Output should be in the same style as the following 
 
@@ -23,3 +23,76 @@
 	#		george100		01/25/2013		00:23:27	d3.js visualization	struggz			
 	
 # 5) Make sure we can read your code!
+
+import os, sys; sys.path.insert(0, os.path.join("..", ".."))
+import unicodedata, time, re
+
+from pattern.web import Twitter, hashtags
+from pattern.db  import Datasheet, pprint
+
+# This example retrieves tweets containing given keywords from Twitter (http://twitter.com).
+
+try: 
+    # We store tweets in a Datasheet that can be saved as a text file (comma-separated).
+    # In the first column, we'll store a unique ID for each tweet.
+    # We only want to add the latest tweets, i.e., those we haven't previously encountered.
+    # With an index on the first column we can quickly check if an ID already exists.
+    # The index becomes important once more and more rows are added to the table (speed).
+    
+    #***************************************************************************
+    # Changed file output location - not sure this is needed?
+    #***************************************************************************
+    table = Datasheet.load("twitter_output.csv")
+    index = dict.fromkeys(table.columns[0], True)
+except:
+    table = Datasheet()
+    index = {}
+
+engine = Twitter(language="en")
+
+# With cached=False, a live request is sent to Twitter,
+# so we get the latest results for the query instead of those in the local cache.
+
+#*******************************************************************************
+# Changed the following:
+#	1. Search pattern
+#	2. Number of tweets to return
+# 	3. Order that the results are returned in
+#	4. Date/Time formatting
+#*******************************************************************************
+for tweet in engine.search("visualization", count=100, cached=False):
+	
+	dateTime = time.strptime(tweet.date, "%a, %d %b %Y %H:%M:%S +0000")
+	#date = time.strftime('%m/%d/%y', dateTime)
+	#time = time.strftime('%H:%M:%S', dateTime)
+
+	print tweet.author.encode('ascii', 'ignore') + "," + \
+		  time.strftime('%m/%d/%y', dateTime).encode('ascii', 'ignore') + "," + \
+		  time.strftime('%H:%M:%S', dateTime).encode('ascii','ignore') + "," + \
+		  tweet.text.encode('ascii', 'ignore') 
+		  
+	#print tweet.text
+	#print tweet.date
+	#print hashtags(tweet.text) # Keywords in tweets start with a #.
+	#print
+	# Create a unique ID based on the tweet content and author.
+	id = str(hash(tweet.author + tweet.text))
+    # Only add the tweet to the table if it doesn't already contain this ID.
+	if len(table) == 0 or id not in index:
+		table.append([id, tweet.text])
+		index[id] = True
+
+table.save("twitter_output.csv")
+
+print "Total results:", len(table)
+print
+
+# Print all the rows in the table.
+# Since it is stored as a file it can grow comfortably each time the script runs.
+# We can also open the table later on, in other scripts, for further analysis.
+#pprint(table)
+
+# Note: you can also search tweets by author:
+# Twitter().search("from:tom_de_smedt")
+
+#print Twitter().search("from:christiestep527")[0].text
