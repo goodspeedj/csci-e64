c0a804238369daa01156b3b865b8e07c210415f9
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-02-11 21:29:29 -0500

    All fields showing up and file being written to as expected...needs some cleanup

--- homework/hw2/API_scraper.py
+++ homework/hw2/API_scraper.py
@@ -25,14 +25,16 @@
 # 5) Make sure we can read your code!
 
 import os, sys; sys.path.insert(0, os.path.join("..", ".."))
-import unicodedata, time, re
+import unicodedata, time, re, csv
 
 from pattern.web import Twitter, hashtags
 from pattern.db  import Datasheet, pprint
 
+
+
 # This example retrieves tweets containing given keywords from Twitter (http://twitter.com).
 
-#try: 
+try: 
     # We store tweets in a Datasheet that can be saved as a text file (comma-separated).
     # In the first column, we'll store a unique ID for each tweet.
     # We only want to add the latest tweets, i.e., those we haven't previously encountered.
@@ -40,13 +42,13 @@ from pattern.db  import Datasheet, pprint
     # The index becomes important once more and more rows are added to the table (speed).
     
     #***************************************************************************
-    # Changed file output location - not sure this is needed?
+    # Changed file output location
     #***************************************************************************
-    #table = Datasheet.load("twitter_output.csv")
-    #index = dict.fromkeys(table.columns[0], True)
-#except:
-    #table = Datasheet()
-    #index = {}
+    table = Datasheet.load("twitter_output.csv")
+    index = dict.fromkeys(table.columns[0], True)
+except:
+    table = Datasheet()
+    index = {}
 
 engine = Twitter(language="en")
 
@@ -60,30 +62,71 @@ engine = Twitter(language="en")
 # 	3. Order that the results are returned in
 #	4. Date/Time formatting
 #*******************************************************************************
-for tweet in engine.search("visualization", count=100, cached=False):
-	
+for tweet in engine.search("visualization", count=100, start=1, cached=False):
+
+	# Parse the date time field	
 	dateTime = time.strptime(tweet.date, "%a, %d %b %Y %H:%M:%S +0000")
-	#date = time.strftime('%m/%d/%y', dateTime)
-	#time = time.strftime('%H:%M:%S', dateTime)
+	
+	#===========================================================================
+	# In the sample output it looks like there are fields for three hashtags
+	# and that if there are no hashtags the fields are populated with 'None'
+	#===========================================================================
+	tags = hashtags(tweet.text)
+	
+	# Trim the list to three entries to match sample.csv
+	if (len(tags) > 3):
+		del tags[3:]
+	
+	
+	# Add 'None' for hashtag fields that are empty - match sample.csv
+	if (len(tags) == 2):
+		tags.append('None')
+	elif (len(tags) == 1):
+		tags.append('None')
+		tags.append('None')
+	elif (len(tags) == 0):
+		tags.append('None')
+		tags.append('None')
+		tags.append('None')
+	else:
+		pass
+	
+	
+	
+	
+	
+	author = tweet.author.encode('ascii', 'ignore')
+	# For some reason assigning these to a variable causes errors - annoying
+	#date = str(time.strftime('%m/%d/%y', dateTime).encode('ascii', 'ignore'))
+	#time = str(time.strftime('%H:%M:%S', dateTime).encode('ascii','ignore'))
+	text = tweet.text.encode('ascii', 'ignore')
+	tag1 = re.sub('#','',tags[0].encode('ascii', 'ignore'))
+	tag2 = re.sub('#','',tags[1].encode('ascii', 'ignore'))
+	tag3 = re.sub('#','',tags[2].encode('ascii', 'ignore'))
 
 	print tweet.author.encode('ascii', 'ignore') + "," + \
 		  time.strftime('%m/%d/%y', dateTime).encode('ascii', 'ignore') + "," + \
 		  time.strftime('%H:%M:%S', dateTime).encode('ascii','ignore') + "," + \
-		  tweet.text.encode('ascii', 'ignore') 
+		  tweet.text.encode('ascii', 'ignore') + "," + \
+		  tags[0].encode('ascii', 'ignore') + "," + \
+		  tags[1].encode('ascii', 'ignore') + "," + \
+		  tags[2].encode('ascii', 'ignore')
 		  
 
 	#print hashtags(tweet.text) # Keywords in tweets start with a #.
 
 	# Create a unique ID based on the tweet content and author.
-	#id = str(hash(tweet.author + tweet.text))
+	id = str(hash(tweet.author + tweet.text))
     # Only add the tweet to the table if it doesn't already contain this ID.
-	#if len(table) == 0 or id not in index:
-	#	table.append([id, tweet.text])
-	#	index[id] = True
+	if len(table) == 0 or id not in index:
+		table.append([author, time.strftime('%m/%d/%y', dateTime).encode('ascii', 'ignore'), \
+					  time.strftime('%H:%M:%S', dateTime).encode('ascii','ignore'), \
+					  text, tag1, tag2, tag3])
+		index[id] = True
 
-#table.save("twitter_output.csv")
+table.save("twitter_output.csv")
 
-#print "Total results:", len(table)
+print "Total results:", len(table)
 print
 
 # Print all the rows in the table.
