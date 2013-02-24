8d6a211b7bd1d1887dc2491333bd6345432902d4
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-02-24 12:12:14 -0500

    Latest script version - does everything but get the per capita information

--- projects/p1/bin/generateCSV.py
+++ projects/p1/bin/generateCSV.py
@@ -1,23 +1,62 @@
 import csv
 import os
-from xml.dom.minidom import parse, parseString
 from lxml import etree
 
 
+# Make sure the required directories exist
+def createDir(dir):
+	if not os.path.exists(dir):
+		os.makedirs(dir)
+		
+
+createDir('../xmldata')
+createDir('../csvdata')
+
+# The raw zip code file
+rawFile = csv.reader(open("../csvdata/free-zipcode-database-Primary.csv"))
+
 # The csv file containing the data (including zip codes) for NH towns
-townFile = csv.reader(open("../data/NHZips.csv"))
+townFile = open('../csvdata/NHZips.csv', 'a')
+
+townFile.write("ZipCode,City,State,Latitude,Longitude,RepubTotal,DemTotal,PerCapita\n")
 
 
-# Pull down all the NH town campaign data from the NYTimes API by zip code
-#for row in townFile:
-#	zip = row[0]
-#	print zip
-#	os.system("wget -O ../data/" + zip + ".xml http://api.nytimes.com/svc/elections/us/v3/finances/2012/president/zips/" + zip + ".xml?api-key=61cbba8d89e40fd80008ba748895e96e:15:66713519")
+# =====================================================================================================
+#
+# Loop through the raw file and pull out the important elements only for NH towns:
+#	- zip code
+# 	- city
+#	- state
+# 	- longitude
+#	- latitude
+#
+# Next use the zip code field to pull down data from the NY Times API
+# 
+# Using lxml and xpath get the Democratic and Republican donation amounts for the NH town
+#
+# Write all this information to the new csv data file
+#
+# =====================================================================================================
+for row in rawFile:
+	if row[3] == 'NH':
+		zipcode  =  row[0]
+		city     =  row[2]
+		state    =  row[3]
+		latitude =  row[5]
+		longitude = row[6]
+		
+		#os.system("wget --quiet -O ../xmldata/" + zipcode + ".xml http://api.nytimes.com/svc/elections/us/v3/finances/2012/president/zips/" + zipcode + ".xml?api-key=61cbba8d89e40fd80008ba748895e96e:15:66713519")
+		print "Pulling NYTimes API data for " + zipcode + " zip code."
+	
+		print "Calculating Democratic and Republican donation amounts for " + city
+		
+		tree = etree.parse('../xmldata/' + zipcode + '.xml')
+		rTotal = tree.xpath("sum(//results/candidates/candidate/total[../party/text() = 'R'])")
+		dTotal = tree.xpath("sum(//results/candidates/candidate/total[../party/text() = 'D'])")
+	
+	
+		townFile.write(zipcode + ',' + city + ',' + state + ',' + str(latitude) + ',' + str(longitude) + ',' + str(rTotal) + ',' + str(dTotal) + '\n')
+	
+	
 
-
-tree = etree.parse("../data/03031.xml")
-
-rTotal = tree.xpath("sum(//results/candidates/candidate/total[../party/text() = 'R'])")
-dTotal = tree.xpath("sum(//results/candidates/candidate/total[../party/text() = 'D'])")
-print rTotal
-print dTotal
+townFile.close()
