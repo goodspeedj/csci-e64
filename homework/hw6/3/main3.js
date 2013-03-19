cd9ed0f0942e59ad49792c74dce4be210eb04688
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-03-19 14:08:33 -0400

    Final versions - colors working by population.

--- homework/hw6/3/main3.js
+++ homework/hw6/3/main3.js
@@ -15,6 +15,25 @@ function iterate() {
 	var json       = JSON.parse(data_json);
 
     for (var i = 0; i < json.length; i++) {
-    	changeColor(json[i].COUNTRY, "green");
+    	var pop = json[i].POPULATION;
+
+    	if (pop > 119553334) {
+    		changeColor(json[i].COUNTRY, "#BD0026");
+    	}
+    	else if (pop > 95706668 && pop <= 119553334) {
+    		changeColor(json[i].COUNTRY, "#F03B20");
+    	}
+    	else if (pop > 71860002 && pop <= 95706668) {
+    		changeColor(json[i].COUNTRY, "#FD8D3C");
+    	}
+    	else if (pop > 48013336 && pop <= 71860002) {
+    		changeColor(json[i].COUNTRY, "#FECC5C");
+    	}
+    	else if (pop > 24166670 && pop <= 48013336) {
+    		changeColor(json[i].COUNTRY, "#FECC5C");
+    	}
+    	else {
+    		changeColor(json[i].COUNTRY, "#FFFFB2");
+    	}
     }
 }
