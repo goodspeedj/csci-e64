e80fb4e7cfa28632c890ecb7d26b700e0c689006
goodspeed <JGoodspeed@bottomline.com>
2013-03-21 16:56:44 -0400

    Minor changes to the display

--- homework/hw6/3/main3.js
+++ homework/hw6/3/main3.js
@@ -20,17 +20,17 @@ function iterate() {
     	if (pop > 119553334) {
     		changeColor(json[i].COUNTRY, "#BD0026");
     	}
-    	else if (pop > 95706668 && pop <= 119553334) {
+    	else if (pop > 81945999 && pop <= 119553334) {
     		changeColor(json[i].COUNTRY, "#F03B20");
     	}
-    	else if (pop > 71860002 && pop <= 95706668) {
+    	else if (pop > 71860002 && pop <= 81946000) {
     		changeColor(json[i].COUNTRY, "#FD8D3C");
     	}
     	else if (pop > 48013336 && pop <= 71860002) {
     		changeColor(json[i].COUNTRY, "#FEB24C");
     	}
     	else if (pop > 24166670 && pop <= 48013336) {
-    		changeColor(json[i].COUNTRY, "#FED");
+    		changeColor(json[i].COUNTRY, "#FED976");
     	}
     	else {
     		changeColor(json[i].COUNTRY, "#FFFFB2");
